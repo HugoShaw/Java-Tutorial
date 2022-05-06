9c05874f40bdb1648df132589994e82872bae013
XWX <44939548+HugoShaw@users.noreply.github.com>
2022-05-06 18:03:55 -0500

    Add files via upload

---
+++ studio-00-xuexiaoyangwrinnye/src/studio0/s.java
@@ -0,0 +1,16 @@
+package studio0;
+
+import studio0.Timing;
+
+public class s {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = "I-love you";
+		System.out.println(x.split("-")[1]);
+		System.out.println(Timing.getCurrentTimeInSeconds());
+	}
+
+}
+
+
